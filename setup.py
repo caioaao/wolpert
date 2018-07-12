d950a13772be012a5d531c0ce41b472b2ff8d62c
caio <caioaao@gmail.com>
2018-07-12 13:54:51 -0300

    setup.py and readme placeholder

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="wolpert",
+      description="Stacked generalization framework",
+      url="https://github.com/caioaao/wolpert",
+      author="Caio Oliveira",
+      license="new BSD",
+      classifiers=['Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development',
+                   'Topic :: Scientific/Engineering',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6'],
+      package_dir={'wolpert': 'wolpert'},
+      packages=find_packages(include=['wolpert', 'wolpert.*']),
+      version="0.0.1",
+      install_requires=["scikit-learn>=0.19"],
+      include_package_data=True,
+      zip_safe=False)
