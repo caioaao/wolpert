43ea27cddb31052c014fedfa5201e4ec10d214f2
Caio Oliveira <caio.oliveira@nubank.com.br>
2018-07-26 16:39:34 -0300

    bugfixes and tests

--- wolpert/wrappers/tests/time_series_test.py
+++ wolpert/wrappers/tests/time_series_test.py
@@ -1,9 +1,20 @@
 import numpy as np
 import pytest
 
-from sklearn.utils.testing import assert_array_equal
+from copy import deepcopy
 
-from wolpert.wrappers.time_series import TimeSeriesSplit
+from sklearn import datasets
+from sklearn.utils.testing import (assert_equal, assert_array_equal,
+                                   assert_almost_equal)
+from sklearn.model_selection import ParameterGrid
+from sklearn.linear_model import RidgeClassifier, LinearRegression
+from sklearn.svm import LinearSVC, LinearSVR
+from sklearn.ensemble import RandomForestClassifier
+
+from wolpert.wrappers.time_series import (TimeSeriesSplit,
+                                          TimeSeriesStackableTransformer)
+
+from .utils import check_estimator
 
 
 def assert_splits_equal(a, b):
@@ -54,4 +65,89 @@ def test_split_class():
         next(splits)
 
 
+RANDOM_SEED = 8939
 
+X, y = datasets.make_classification(random_state=RANDOM_SEED, n_samples=200)
+
+META_ESTIMATOR_PARAMS = {'offset': [0, 1],
+                         'test_set_size': [1, 20],
+                         'min_train_size': [30, 50],
+                         'max_train_size': [None, 60]}
+
+
+def _check_estimator(estimator, **fit_params):
+    # basic checks
+    check_estimator(estimator, X, y, **fit_params)
+
+    Xt, indexes = estimator.blend(X, y, **fit_params)
+
+
+def test_regression():
+    # tests regression with various parameter settings
+
+    meta_params = {'method': ['auto', 'predict']}
+    meta_params.update(META_ESTIMATOR_PARAMS)
+
+    regressors = [LinearRegression(), LinearSVR(random_state=RANDOM_SEED)]
+
+    for reg in regressors:
+        for params in ParameterGrid(meta_params):
+            blended_reg = TimeSeriesStackableTransformer(reg, **params)
+            _check_estimator(blended_reg)
+
+
+def test_classification():
+    # tests classification with various parameter settings
+
+    testcases = [{'clf': RandomForestClassifier(random_state=RANDOM_SEED),
+                  'extra_params': {'method': ['auto', 'predict',
+                                              'predict_proba']}},
+                 {'clf': LinearSVC(random_state=RANDOM_SEED),
+                  'extra_params': {'method': ['auto', 'predict',
+                                              'decision_function']}},
+                 {'clf': RidgeClassifier(random_state=RANDOM_SEED),
+                  'extra_params': {'method': ['auto', 'predict']}}]
+
+    for testcase in testcases:
+        clf = testcase['clf']
+
+        meta_params = deepcopy(testcase['extra_params'])
+        meta_params.update(META_ESTIMATOR_PARAMS)
+
+        for params in ParameterGrid(meta_params):
+            blended_clf = TimeSeriesStackableTransformer(clf, **params)
+            _check_estimator(blended_clf)
+
+
+WRAPPER_PARAMS = {"default_method": ["auto", "predict_proba"],
+                  "holdout_size": [.1, .2],
+                  "random_state": [10, 20, 30],
+                  "fit_to_all_data": [True, False]}
+
+
+# def test_wrapper():
+#     dummy_estimator = "dummy"
+
+#     for params in ParameterGrid(WRAPPER_PARAMS):
+#         wrapper = TimeSeriesSplitWrapper(**params)
+
+#         default_method = params.pop("default_method")
+
+#         for method in [None, "auto", "predict_proba"]:
+#             wrapped_est = wrapper.wrap_estimator(
+#                 dummy_estimator, method=method)
+
+#             direct_wrapped_est = TimeSeriesStackableTransformer(
+#                 dummy_estimator, method=method, **params)
+
+#             # checks that method is chosen appropriately
+#             if method is None:
+#                 assert_equal(wrapped_est.method, default_method)
+#             else:
+#                 assert_equal(wrapped_est.method, method)
+
+#             # check that both transformers are the same
+#             for k in ["estimator", "holdout_size", "random_state",
+#                       "fit_to_all_data"]:
+#                 assert_equal(getattr(wrapped_est, k),
+#                              getattr(direct_wrapped_est, k))
--- wolpert/wrappers/time_series.py
+++ wolpert/wrappers/time_series.py
@@ -1,11 +1,148 @@
 import numpy as np
+import scipy.sparse as sp
 
-from sklearn.base import clone
 from sklearn.utils import indexable
 from sklearn.utils.validation import _num_samples
-from sklearn.model_selection._split import _BaseKFold
+from sklearn.model_selection._validation import _fit_and_predict
+from sklearn.model_selection._split import check_cv
+from sklearn.base import is_classifier, clone
+from sklearn.preprocessing import LabelEncoder
+from sklearn.externals.joblib import Parallel, delayed, logger
+
 
 from .base import BaseStackableTransformer, BaseWrapper
+
+
+def _cross_val_predict(estimator, X, y=None, groups=None, cv=None, n_jobs=1,
+                       verbose=0, fit_params=None, pre_dispatch='2*n_jobs',
+                       method='predict'):
+    """Generate cross-validated estimates for each input data point
+
+    Copied from scikit learn, with the only difference that it doesn't check for
+    permutations.
+
+    Parameters
+    ----------
+    estimator : estimator object implementing 'fit' and 'predict'
+        The object to use to fit the data.
+
+    X : array-like
+        The data to fit. Can be, for example a list, or an array at least 2d.
+
+    y : array-like, optional, default: None
+        The target variable to try to predict in the case of
+        supervised learning.
+
+    groups : array-like, with shape (n_samples,), optional
+        Group labels for the samples used while splitting the dataset into
+        train/test set.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+
+        - None, to use the default 3-fold cross validation,
+        - integer, to specify the number of folds in a `(Stratified)KFold`,
+        - An object to be used as a cross-validation generator.
+        - An iterable yielding train, test splits.
+
+        For integer/None inputs, if the estimator is a classifier and ``y`` is
+        either binary or multiclass, :class:`StratifiedKFold` is used. In all
+        other cases, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validation strategies that can be used here.
+
+    n_jobs : integer, optional
+        The number of CPUs to use to do the computation. -1 means
+        'all CPUs'.
+
+    verbose : integer, optional
+        The verbosity level.
+
+    fit_params : dict, optional
+        Parameters to pass to the fit method of the estimator.
+
+    pre_dispatch : int, or string, optional
+        Controls the number of jobs that get dispatched during parallel
+        execution. Reducing this number can be useful to avoid an
+        explosion of memory consumption when more jobs get dispatched
+        than CPUs can process. This parameter can be:
+
+            - None, in which case all the jobs are immediately
+              created and spawned. Use this for lightweight and
+              fast-running jobs, to avoid delays due to on-demand
+              spawning of the jobs
+
+            - An int, giving the exact number of total jobs that are
+              spawned
+
+            - A string, giving an expression as a function of n_jobs,
+              as in '2*n_jobs'
+
+    method : string, optional, default: 'predict'
+        Invokes the passed method name of the passed estimator. For
+        method='predict_proba', the columns correspond to the classes
+        in sorted order.
+
+    Returns
+    -------
+    predictions : ndarray
+        This is the result of calling ``method``
+
+    See also
+    --------
+    cross_val_score : calculate score for each CV split
+
+    cross_validate : calculate one or more scores and timings for each CV split
+
+    Notes
+    -----
+    In the case that one or more classes are absent in a training portion, a
+    default score needs to be assigned to all instances for that class if
+    ``method`` produces columns per class, as in {'decision_function',
+    'predict_proba', 'predict_log_proba'}.  For ``predict_proba`` this value is
+    0.  In order to ensure finite output, we approximate negative infinity by
+    the minimum finite float value for the dtype in other cases.
+
+    Examples
+    --------
+    >>> from sklearn import datasets, linear_model
+    >>> from sklearn.model_selection import cross_val_predict
+    >>> diabetes = datasets.load_diabetes()
+    >>> X = diabetes.data[:150]
+    >>> y = diabetes.target[:150]
+    >>> lasso = linear_model.Lasso()
+    >>> y_pred = cross_val_predict(lasso, X, y)
+
+    """
+    X, y, groups = indexable(X, y, groups)
+
+    cv = check_cv(cv, y, classifier=is_classifier(estimator))
+
+    if method in ['decision_function', 'predict_proba', 'predict_log_proba']:
+        le = LabelEncoder()
+        y = le.fit_transform(y)
+
+    # We clone the estimator to make sure that all the folds are
+    # independent, and that it is pickle-able.
+    parallel = Parallel(n_jobs=n_jobs, verbose=verbose,
+                        pre_dispatch=pre_dispatch)
+    prediction_blocks = parallel(delayed(_fit_and_predict)(
+        clone(estimator), X, y, train, test, verbose, fit_params, method)
+        for train, test in cv.split(X, y, groups))
+
+    # Concatenate the predictions
+    predictions = [pred_block_i for pred_block_i, _ in prediction_blocks]
+    test_indices = np.concatenate([indices_i
+                                   for _, indices_i in prediction_blocks])
+
+    # Check for sparse predictions
+    if sp.issparse(predictions[0]):
+        predictions = sp.vstack(predictions, format=predictions[0].format)
+    else:
+        predictions = np.concatenate(predictions)
+    return predictions, test_indices
 
 
 class TimeSeriesSplit:
@@ -82,12 +219,12 @@ class TimeSeriesSplit:
         n_samples = _num_samples(X)
         indices = np.arange(n_samples)
         train_ends = range(self.min_train_size,
-                           n_samples - (self.test_set_size + self.offset) + 1)
+                           n_samples - (self.test_set_size + self.offset))
 
         for train_end in train_ends:
             test_start = train_end + self.offset
             test_end = train_end + self.offset + self.test_set_size
-            if self.max_train_size and self.max_train_size <= test_start:
+            if self.max_train_size and self.max_train_size < train_end:
                 train_start = train_end - self.max_train_size
             else:
                 train_start = 0
@@ -120,22 +257,85 @@ class TimeSeriesStackableTransformer(BaseStackableTransformer):
         Size of the test set. This will also be the amount of rows added to the
         training set at each iteration
 
-    n_splits : int, optional (default=None)
-        Number of splits. Must be at least 1. If ``None``, will use all
-        available data.
-
     min_train_size : int, optional (default=1)
         Minimum size for a single training set.
 
     max_train_size : int, optional (default=None)
         Maximum size for a single training set.
+
+    n_cv_jobs : int, optional (default=1)
+        Number of jobs to be passed to ``cross_val_predict`` during
+        ``blend``.
+
     """
     def __init__(self, estimator, method='auto', offset=0, test_set_size=1,
-                 n_splits=None, min_train_size=1, max_train_size=None):
+                 min_train_size=1, max_train_size=None, n_cv_jobs=1):
         super(TimeSeriesStackableTransformer, self).__init__(estimator, method)
         self.offset = offset
         self.test_set_size = test_set_size
-        self.n_splits = n_splits
         self.min_train_size = min_train_size
         self.max_train_size = max_train_size
+        self.n_cv_jobs = n_cv_jobs
 
+    def _splitter(self):
+        return TimeSeriesSplit(
+            offset=self.offset, test_set_size=self.test_set_size,
+            min_train_size=self.min_train_size,
+            max_train_size=self.max_train_size)
+
+    def blend(self, X, y, **fit_params):
+        """Transform dataset using time series split.
+
+        Parameters
+        ----------
+        X : array-like or sparse matrix, shape=(n_samples, n_features)
+            Input data used to build forests. Use ``dtype=np.float32`` for
+            maximum efficiency.
+
+        y : array-like, shape = [n_samples]
+            Target values.
+
+        **fit_params : parameters to be passed to the base estimator.
+
+        Returns
+        -------
+        X_transformed, indexes : tuple of (sparse matrix, array-like)
+            `X_transformed` is the transformed dataset.
+            `indexes` is the indexes of the transformed data on the input.
+        """
+        ts = self._splitter()
+
+        self.estimator_ = clone(self.estimator)
+        Xt, indexes = _cross_val_predict(self.estimator_, X, y, cv=ts,
+                                         n_jobs=self.n_cv_jobs,
+                                         method=self._estimator_function_name)
+        self.estimator_ = None
+
+        if Xt.ndim == 1:
+            Xt = Xt.reshape(-1, 1)
+        return Xt, indexes
+
+    def fit_blend(self, X, y, **fit_params):
+        """Transform dataset using cross validation and fits the estimator to the
+        entire dataset.
+
+        Parameters
+        ----------
+        X : array-like or sparse matrix, shape=(n_samples, n_features)
+            Input data used to build forests. Use ``dtype=np.float32`` for
+            maximum efficiency.
+
+        y : array-like, shape = [n_samples]
+            Target values.
+
+        **fit_params : parameters to be passed to the base estimator.
+
+        Returns
+        -------
+        X_transformed, indexes : tuple of (sparse matrix, array-like)
+            `X_transformed` is the transformed dataset.
+            `indexes` is the indexes of the transformed data on the input.
+        """
+        blend_results = self.blend(X, y, **fit_params)
+        self.fit(X, y, **fit_params)
+        return blend_results
